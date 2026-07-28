efa4a4393312e9e81c7a7179c5e401da41b8341a
Sharcbloc749 <bozhetarnikmatthew@gmail.com>
2026-07-28 16:59:39 -0300

    Update uv.config.js

--- URL/uv/uv.config.js
+++ URL/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/URL/uv/service/',
-  bare: 'wss://wisp.mercuryworkshop.sub.id/',
+  bare: 'wss://wisp.mercurywork.shop/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/URL/uv/uv.handler.js',
