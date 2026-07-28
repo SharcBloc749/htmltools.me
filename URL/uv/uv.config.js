0c7cdcfc280e616c9745620436010074aa5c168f
Sharcbloc749 <bozhetarnikmatthew@gmail.com>
2026-07-28 15:57:26 -0300

    Update UV config paths to include 'URL' prefix

--- URL/uv/uv.config.js
+++ URL/uv/uv.config.js
@@ -1,12 +1,12 @@
+/* URL/uv/uv.config.js */
 self.__uv$config = {
-  prefix: '/uv/service/',
-  bare: 'https://my-htmltools-bare.onrender.com/',
-  
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: '/uv/uv.handler.js',
-  client: '/uv/uv.client.js',
-  bundle: '/uv/uv.bundle.js',
-  config: '/uv/uv.config.js',
-  sw: '/uv/uv.sw.js',
+    prefix: '/URL/uv/service/',
+    bare: 'https://my-htmltools-bare.onrender.com/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/URL/uv/uv.handler.js',
+    client: '/URL/uv/uv.client.js',
+    bundle: '/URL/uv/uv.bundle.js',
+    config: '/URL/uv/uv.config.js',
+    sw: '/URL/uv/uv.sw.js',
 };
