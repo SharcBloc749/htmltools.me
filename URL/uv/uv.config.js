cb1fcea547daa3aad3f6172c4573fb14a4703e5a
Sharcbloc749 <bozhetarnikmatthew@gmail.com>
2026-07-28 16:05:05 -0300

    Update uv.config.js

--- URL/uv/uv.config.js
+++ URL/uv/uv.config.js
@@ -1,12 +1,12 @@
-/* URL/uv/uv.config.js */
 self.__uv$config = {
-    prefix: '/URL/uv/service/',
-    bare: 'https://my-htmltools-bare.onrender.com/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/URL/uv/uv.handler.js',
-    client: '/URL/uv/uv.client.js',
-    bundle: '/URL/uv/uv.bundle.js',
-    config: '/URL/uv/uv.config.js',
-    sw: '/URL/uv/uv.sw.js',
+  prefix: '/URL/uv/service/',
+  bare: 'https://my-htmltools-bare.onrender.com/',
+  
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/URL/uv/uv.handler.js',
+  client: '/URL/uv/uv.client.js',
+  bundle: '/URL/uv/uv.bundle.js',
+  config: '/URL/uv/uv.config.js',
+  sw: '/URL/uv/uv.sw.js',
 };
