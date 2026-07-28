70c1401514c2d3f1b4e6af7344780632f5b68d0f
Sharcbloc749 <bozhetarnikmatthew@gmail.com>
2026-07-28 17:08:24 -0300

    Update sw.js

--- URL/sw.js
+++ URL/sw.js
@@ -1,11 +1,11 @@
-importScripts('./uv/uv.bundle.js');
-importScripts('./uv/uv.config.js');
+importScripts('/URL/uv/uv.bundle.js');
+importScripts('/URL/uv/uv.config.js?v=14');
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(location.origin + location.pathname.replace('sw.js', '') + self.__uv$config.prefix)) {
+  if (event.request.url.startsWith(location.origin + self.__uv$config.prefix)) {
     event.respondWith(sw.fetch(event));
   }
 });
