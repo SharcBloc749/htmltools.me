5831860e6186f4b6d5bb185e4ac2ecad9e6f955a
Sharcbloc749 <bozhetarnikmatthew@gmail.com>
2026-07-28 15:40:49 -0300

    Update uv.sw.js

--- uv/uv.sw.js
+++ uv/uv.sw.js
@@ -1,19 +1,11 @@
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.handler.js');
+importScripts('/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
-self.addEventListener('install', () => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-});
-
 self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(location.origin + self.__uv$config.prefix)) {
-        event.respondWith(sw.fetch(event));
-    }
+  if (event.request.url.startsWith(location.origin + self.__uv$config.prefix)) {
+    event.respondWith(sw.fetch(event));
+  }
 });
