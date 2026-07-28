fa4c69f15e46e54b59fe9566cfac7f84ebdca042
Sharcbloc749 <bozhetarnikmatthew@gmail.com>
2026-07-28 16:55:28 -0300

    Update sw.js

--- URL/sw.js
+++ URL/sw.js
@@ -1,7 +1,5 @@
 importScripts('/URL/uv/uv.bundle.js');
-importScripts('/URL/uv/uv.config.js?v=13');
-
-// Load compiled UV service worker core directly from CDN
+importScripts('/URL/uv/uv.config.js');
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.sw.js');
 
 const sw = new UVServiceWorker();
